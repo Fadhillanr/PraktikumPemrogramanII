dd61fcd8e1de9ef4d495a7224cd1a589ceec59d2
Fadhillanr <121225974+Fadhillanr@users.noreply.github.com>
2024-11-05 15:46:36 +0700

    Add files via upload

---
+++ Pertemuan7/Membership/src/view/jenismember/JenisMemberButtonSimpanActionListener.java
@@ -0,0 +1,27 @@
+package tugas7.Membership.src.view.jenismember;
+
+import java.awt.event.*;
+import java.util.UUID;
+import tugas7.Membership.src.model.JenisMember;
+import tugas7.Membership.src.dao.JenisMemberDao;
+
+public class JenisMemberButtonSimpanActionListener implements ActionListener{
+    private JenisMemberFrame jenisMemberFrame;
+    private JenisMemberDao jenisMemberDao;
+
+    public JenisMemberButtonSimpanActionListener(JenisMemberFrame jenisMemberFrame, JenisMemberDao jenisMemberDao){
+        this.jenisMemberFrame = jenisMemberFrame;
+        this.jenisMemberDao = jenisMemberDao;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String nama = this.jenisMemberFrame.getNama();
+        JenisMember jenisMember = new JenisMember();
+        jenisMember.setId(UUID.randomUUID().toString());
+        jenisMember.setNama(nama);
+
+        this.jenisMemberFrame.addJenisMember(jenisMember);
+        this.jenisMemberDao.insert(jenisMember);
+    }
+}
